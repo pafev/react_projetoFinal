98ed2b9361fcea0f8331c5c38279b186065de899
pafev <paulofvilarimda@gmail.com>
2022-12-21 21:06:37 -0300

    css navbar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,11 +7,12 @@ import searchIcon from '../../assets/search-icon.png'
 const Header = () => {
     return (
         <Container>
-            <header className="navTop">
+            <nav className="navTop">
                 <div className="navTopLeft">
                     <img src={logo} alt='logo'></img>
                     <p>E-commerce</p>
                 </div>
+
                 <div className="navTopRight">
                     <div className="Pesquisar">
                         <input placeholder="Pesquise seu produto"></input>
@@ -19,18 +20,20 @@ const Header = () => {
                     </div>
                     <img src={cartIcon} alt='carrinho'></img>
                 </div>
-            </header>
-            <header className="navBottom">
+            </nav>
+
+            <nav className="navBottom">
                 <div className="navBottomLeft">
                     <p>Home</p>
                     <p>About Us</p>
                 </div>
+                
                 <div className="navBottomRight">
                     <p>Entrar</p>
                     <p>Registrar</p>
                     <img src={iconPerfil} alt='perfil'></img>
                 </div>
-            </header>
+            </nav>
         </Container>
     )
 }
--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -1,5 +1,115 @@
 import styled from 'styled-components'
 
 export const Container = styled.header`
+
+nav {
+    position: sticky;
+}
+
+.navTop {
     background-color: #F3F3F3;
+    height: 4.2rem;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+
+.navTop .navTopLeft {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    vertical-align: middle;
+    margin-left: 1.5rem;
+}
+
+.navTop .navTopLeft p {
+    font-size: 1.3rem;
+    font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+.navTop .navTopLeft img {
+    position: relative;
+    width: 2.1rem;
+    height: 2rem;
+    margin-right: 0.5rem;
+}
+
+.navTop .navTopRight {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-right: 2rem;
+}
+
+.navTop .navTopRight img {
+    width: 1.8rem;
+    height: 1.9rem;
+    margin-left: 0.6rem;
+}
+
+.navTop .Pesquisar {
+    display: flex;
+    background-color: white;
+    height: 2rem;
+    width: 26rem;
+    justify-content: space-between;
+    padding: 0.5rem;
+    border-radius: 2px;
+    margin-right: 1rem;
+    border: 1px solid rgba(0, 0, 0, 0.5);
+}
+
+.navTop .Pesquisar input {
+    outline: none;
+    border: none;
+    width: 100%;
+    font-size: 15px;
+}
+
+.navTop .Pesquisar img {
+    height: 1.1rem;
+    width: 1.1rem;
+    align-self: center;
+    opacity: 0.6;
+}
+
+.navBottom {
+    color: white;
+    background-color: black;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 2.6rem;
+    font-weight:600;
+    font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+.navBottomRight {
+    display: flex;
+    flex-direction: row;
+}
+
+.navBottomLeft {
+    display: flex;
+    flex-direction: row;
+    margin: 0.6rem 0 0 1.3rem;
+}
+
+.navBottomRight {
+    display: flex;
+    flex-direction: row;
+    margin: 0.6rem 2.2rem 0 0rem;
+}
+
+.navBottom p{
+    margin: 0 0.5rem;
+}
+
+.navBottomRight img {
+    width: 1.1rem;
+    height: 1.1rem;
+    margin-left: 0.5rem;
+}
+
 `
