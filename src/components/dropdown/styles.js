7bbb1452caabef40c3e817d78f39ea071d34c615
pafev <paulofvilarimda@gmail.com>
2022-12-24 15:49:21 -0300

    adjusting component dropdown

--- src/components/dropdown/styles.js
+++ src/components/dropdown/styles.js
@@ -2,61 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 img {
-    height: 2rem;
-}
-
-.dropdownBtn {
-    height: 100%;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    background-color: black;
-    outline: none;
-    color: white;
-    font-size: 20px;
-    cursor: pointer;
-    font-family: 'Times New Roman', Times, sans-serif;
-    font-weight: bold;
-}
-
-.dropdown {
-    height: 100%;
-    width: 8rem;
-    position: relative;
-    display: inline-block;
-    border-bottom: 3px solid white;
-    margin: 0rem 0.8rem;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  left: -10%;
-  background-color: #f9f9f9;
-  width: 120%;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-
-#contentShow {
-    display: block;
-}
-
-.dropdown-content .item {
-    background-color: rgba(0, 0, 0, 0.9);
-    color: white;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-}
-
-.dropdown-content .item:hover {color: rgba(255, 255, 255, 0.8)}
-
-.dropdownBtn p {
-    display: flex;
-    align-items: center;
-    margin: 0 auto;
-    position: relative;
-    left: 0.5rem;
+    height: 1.5rem;
 }
 `
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -42,6 +42,8 @@ export const Container = styled.section`
     /* justify-content: space-around; */
 }
 
+// ajustes do dropdown
+
 .filters {
     position: relative;
     top: -2.5rem;
@@ -53,6 +55,61 @@ export const Container = styled.section`
     flex-wrap: wrap;
     background-color: black;
     box-shadow: 0px 3px 9px rgba(0,0,0,0.6);
+}
+
+.dropdownBtn {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    background-color: black;
+    outline: none;
+    color: white;
+    font-size: 20px;
+    cursor: pointer;
+    font-family: 'Times New Roman', Times, sans-serif;
+    font-weight: bold;
+}
+
+.dropdown {
+    height: 100%;
+    width: 8rem;
+    position: relative;
+    display: inline-block;
+    border-bottom: 3px solid white;
+    margin: 0rem 0.8rem;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  left: -10%;
+  background-color: #f9f9f9;
+  width: 120%;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+#contentShow {
+    display: block;
+}
+
+.dropdown-content .item {
+    background-color: rgba(0, 0, 0, 0.9);
+    color: white;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+}
+
+.dropdown-content .item:hover {color: rgba(255, 255, 255, 0.8)}
+
+.dropdownBtn p {
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+    position: relative;
+    left: 0.5rem;
 }
 
 // parte da responsividade do welcome e dos filtros
