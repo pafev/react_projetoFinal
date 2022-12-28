cac4960d2caa52d7e9fde92f43bc054d17c5362a
pafev <paulofvilarimda@gmail.com>
2022-12-28 17:15:01 -0300

    design css loginPage

--- src/pages/loginPage/index.js
+++ src/pages/loginPage/index.js
@@ -1,7 +1,28 @@
+import { Container } from "./styles"
+import { Link } from "react-router-dom"
+import { FaUsers } from 'react-icons/fa'
 
 const LoginPage = () => {
     return (
-        <p>Você está na página de login</p>
+        <Container>
+            <div className="content">
+                <FaUsers className="icon" />
+                <div className="inputData">
+                    <div className="inputContent">
+                        <span>E-mail</span>
+                        <input className="inputEmail" placeholder="Seu e-mail"></input>
+                    </div>
+                    <div className="inputContent">
+                        <span>Senha</span>
+                        <input className="inputSenha" placeholder="Sua senha"></input>
+                    </div>      
+                </div>
+                <div className="buttons">
+                    <button className="enter">Entrar</button>
+                    <Link to="/registrar" className="register">Ainda não sou cliente</Link>
+                </div>
+            </div>
+        </Container>
     )
 }
 
---
+++ src/pages/loginPage/styles.js
@@ -0,0 +1,130 @@
+import styled from "styled-components";
+import backLogin from '../../assets/background-login-register.jpg'
+
+export const Container = styled.section`
+background-image: url(${backLogin});
+background-size: 100%;
+background-repeat: no-repeat;
+background-position-y: 30%;
+width: 100vw;
+height: 130vh;
+display: flex;
+justify-content: center;
+
+.content {
+    background-color: rgba(0, 0, 0, 0.3);
+    height: 22rem;
+    width: 24rem;
+    margin: auto 0;
+    display: flex;
+    flex-direction: column;
+    border-radius: 1rem;
+
+    > .icon {
+        font-size: 75px;
+        position: relative;
+        margin: 0 auto;
+        top: -2.7rem;
+        color: white;
+    }
+
+    > .inputData {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        position: relative;
+        top: -1.5rem;
+
+        > .inputContent {
+            display: flex;
+            flex-direction: column;
+            width: 75%;
+            font-family: Neucha, sans-serif;
+
+            > span {
+                color: white;
+                font-weight: 500;
+            }
+
+            > input {
+                font-size: 14px;
+                height: 2.2rem;
+                padding: 0 0.4rem;
+                color: #818489;
+                border: none;
+                border-radius: 2px;
+                outline: none;
+                &:focus{
+                    &::-webkit-input-placeholder{
+                        transition: opacity 0.25s ease; 
+                        opacity: 0;
+                    }
+                    &::-moz-placeholder {
+                        transition: opacity 0.25s ease; 
+                        opacity: 0;
+                    }
+                    &:-ms-placeholder {
+                        transition: opacity 0.25s ease; 
+                        opacity: 0;
+                    }    
+                }
+            }
+        }
+
+        > .inputContent + .inputContent {
+            margin-top: 1.2rem;
+        }
+    }
+
+    > .buttons {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 0.5rem;
+        font-family: Neucha, sans-serif;
+
+        > button {
+            position: relative;
+            height: 2.8rem;
+            width: 13rem;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: white;
+            outline: none;
+            border: none;
+            border-radius: 5px;
+            font-size: 1.2rem;
+            font-weight: bold;
+            background-position: 0 90%;
+            box-shadow: rgba(0, 0, 0, .2) 15px 28px 25px -18px;
+            color: #41403e;
+            cursor: pointer;
+            transition: all 235ms ease-in-out;
+            user-select: none;
+            -webkit-user-select: none;
+            touch-action: manipulation;
+        }
+
+        > button:hover {
+            box-shadow: rgba(0, 0, 0, .3) 2px 8px 8px -5px;
+            transform: translateY(1px);
+        }
+
+        > button:focus {
+            box-shadow: rgba(0, 0, 0, .3) 2px 8px 4px -6px;
+        }
+
+        > button:active {
+            transform: translateY(2px);
+        }
+
+        > .register {
+            font-weight: lighter;
+            margin-top: 1.2rem;
+            color: white;
+            font-style: italic;
+        }
+    }
+}
+`
