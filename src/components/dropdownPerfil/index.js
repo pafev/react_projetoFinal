5133c6805f5b0d00975e36313b40cd78e41ca94c
pafev <paulofvilarimda@gmail.com>
2022-12-28 20:06:35 -0300

    aplicando useContext na navbar

--- src/components/dropdownPerfil/index.js
+++ src/components/dropdownPerfil/index.js
@@ -4,6 +4,7 @@ import { Container } from "./styles"
 import iconPerfil from '../../assets/perfil-icon-white.png'
 import { BiEdit } from 'react-icons/bi'
 import { FiLogOut } from 'react-icons/fi'
+import { useUserContext } from "../../context/useUserContext"
 
 const DropdownPerfil = () => {
     let contentRef = useRef()
@@ -18,6 +19,8 @@ const DropdownPerfil = () => {
         document.addEventListener('mousedown', handler)
     },[])
 
+    const {user} = useUserContext()
+
     return(
         <Container>
         <div className="dropdown" ref={contentRef}>
@@ -25,19 +28,28 @@ const DropdownPerfil = () => {
                 <img src={iconPerfil} alt='perfil'></img>
             </button>
             <div className="dropdown-content" id={openDropdown? 'contentShow' : ''}>
-                <h1>Nome do usuário</h1>
-                <h2>R$00,00</h2>
-                <Link className="item" to='/perfil'>
-                    Editar perfil
-                    <BiEdit className="icon"/>
-                </Link>
-                <Link className="item" to='/'>
-                    Admin
-                </Link>
-                <Link className="item" to='/'>
-                    Logout
-                    <FiLogOut className="icon logout"/>
-                </Link>
+                {user?
+                 <>
+                    <h1>
+                        {user? user.name : "Usuário não logado"}
+                    </h1>
+                    <h2>
+                        {user? `R$ ${(parseFloat(user.credit_wallet) / 100).toFixed(2)}` : "" }
+                    </h2>
+                    <Link className="item" to='/perfil'>
+                        Editar perfil
+                        <BiEdit className="icon"/>
+                    </Link>
+                    <Link className="item" to='/'>
+                        Admin
+                    </Link>
+                    <Link className="item" to='/'>
+                        Logout
+                        <FiLogOut className="icon logout"/>
+                    </Link>
+                 </> :
+                <h1>Usuário não logado</h1>
+                }
             </div>
         </div>
         </Container>
