754aadd40e897b4aaf742af79141ae93843a0b9a
Dan <danieljosepalmar@gmail.com>
2023-01-12 18:49:31 -0300

    productPage structure middle

--- src/pages/productPage/index.js
+++ src/pages/productPage/index.js
@@ -1,35 +1,125 @@
 import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 import { api } from "../../services/api"
 import { Container } from "./styles"
+import { photo } from "../../services/photo"
+import noImage_url from '../../assets/no-image_url.jpg'
+import {MdKeyboardArrowRight} from "react-icons/md"
+import {BsBagCheck} from "react-icons/bs"
 
-const ProductPage =()=> {
-    const [product, setProduct] = useState([])
-    useEffect(() => {
-      api.get('/products/show/:id').then(response => setProduct(response.data))
+// import { useUserContext } from "../../context/useUserContext"
 
-    }, [])
+
+const ProductPage = () => {
+
+    const {id} = useParams()
+
+    // const {user} = useUserContext()
+
+    const [product, setProduct] = useState({})
+
+    // const [foto, setFoto] = useState(0)
     
-    return(
+    const [buyQuantity, setBuyQuantity] = useState(1)
 
+    const purchase = () => {console.log("batata")}
+
+    useEffect(() => {
+        api.get(`/products/show/${id}`).then(response => setProduct(response.data))
+    },[id])
+    console.log(product)
+    return (
         <Container>
-            <title>              
-                <p> 
-                    Pagina do produto aqui 
-                </p>  
-            </title>
 
-            <div>
-            <div className="nomeProoduto">
-                    <div>
-                        <h1>
-                            Categoria - Nome do Produto
-                        </h1>
+            <div className="nomemarca">
+                
+                {product.brand} <MdKeyboardArrowRight/> {product.name} 
+            </div>
+
+            <div className="productInfo">
+
+                <div className="productPhoto">
+
+                    <div className="photosCascade">  
+                    {product.images_url ?
+                            product.images_url.slice(0,5).map((item, index) => (
+                                <div>   
+                                    <img src = {product.images_url ? photo.defaults.baseURL + product.images_url[index] : noImage_url } 
+                                    alt = 'imagem do produto'/> 
+                                </div>
+                            )) :
+                            <></>} 
+                    </div>
+
+                    <div className="photoMain">
+
+                        <img src={product.images_url ? photo.defaults.baseURL + product.images_url[0] : noImage_url }
+                        alt='imagem do produto'/>
+                    </div>
+                </div>
+
+                <div className="textoProduto">
+                    <h1>{product.name}</h1>
+                    <h4>{product.brand}</h4>
+                    <div className="productDesc">
+                        <h4>
+                            <p>Sobre esse item:</p>
+                            {product.description}
+                        </h4>
+                    </div>
+
+                </div>
+            
+
+                <div className="purchase">
+                    <div className="price">
+                        <h4>
+                            R$
+                        </h4>
+                        <h1>{(parseFloat(product.price) / 100).toFixed(2)}</h1>
+
+                    </div>
+                    <div className="stock">
+                        {product.stock< 1 ? <p>Não está disponível!</p> 
+                        : <><p>Em estoque</p> 
+                        {/* <p>{product.stock} Disponíveis</p> */}
+                        </>}
+                    </div>
+                    <hr/> 
+                    <p>Quantidade</p>  
+                    <div className="buyQuantity">
+                                
+                            <button id="-" onClick ={()=> setBuyQuantity(buyQuantity - 1) }>
+                                -
+                            </button>
+                            
+                            <div className="numQuantity">
+                                {buyQuantity}
+                            </div>
+
+                            <button id="+" onClick ={()=> setBuyQuantity(buyQuantity + 1) }>
+                                + 
+                            </button>
+                        
+                    </div>
+                    <div className="buyButton">
+                        <button onClick = {purchase}>
+                            <BsBagCheck/> Comprar Agora
+
+                        </button>
+                        
                     </div>
                 </div>
             </div>
+
+            <div className brandProducts>
+
+            </div>
+
+
+
         </Container>
     )
-
 }
 
 export default ProductPage
--- src/pages/productPage/styles.js
+++ src/pages/productPage/styles.js
@@ -1,8 +1,97 @@
 import styled from "styled-components";
-export const Container = styled.section`
 
-.nomeProduto{
-    height: 100%
+export const Container = styled.section`
+margin-top: 7rem;
+
+
+.nomemarca {
+    margin-left: 1rem;
+    margin-bottom: 2rem;
 }
+.productInfo{
+    margin-left: 0px;
+    display: flex;
+    direction: row;
+    justify-content: space-between;
+    /* width: 17rem; */
+}
+.productPhoto{
+    margin-left: 0px;
+    display: flex;
+    direction: row;
+    justify-content: space-between;
+    width: 17rem; 
+}
+.photoMain {
+    display:flex;
+    margin-left: 10 rem;
+
+    img{
+        max-height: 10rem;
+        max-width: 10rem
+
+    }
+}
+.photosCascade {
+    display:flex;
+    margin-left: 2rem;
+    flex-direction: column;
+
+    img{
+        height: 5rem;
+        width: 5rem
+
+    }
+}
+
+.textoProduto{
+
+    display: flex;
+    flex-direction: column;
+    margin-left: 7rem;
+
+
+}
+.purchase{
+    background-color: #e6e6e6;
+    display: flex;
+    width: 17rem;
+    flex-direction: column;
+    align-items: center; 
+    /* padding-right: 5rem; */
+    justify-content: space-evenly;
+    margin-right: 3rem;
+    
+    
+
+}
+.buyQuantity{
+    display: flex;
+    flex-direction: row;
+    width: 80%;
+    justify-content: center;
+    background-color: white;
+    /* border: 1px solid black; */
+    border-radius: 10px ;
+   
+    button {
+        width: 35%;
+        background-color: black;
+        color: white;
+        cursor: pointer;
+        height: 100%;
+        border-radius: 10px 
+
+
+    }
+    .numQuantity{
+        
+        display: flex;
+        width: 30%;
+        justify-content: center
+    }
+}
+
+
 
 `
